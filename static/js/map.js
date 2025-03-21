b6f07e522b28845e5ccaa0a6b10a83181c8be51b
skywall34 <doshinkorean@gmail.com>
2025-03-20 21:31:24 -0500

    feat(statistics): Added markers

--- static/js/map.js
+++ static/js/map.js
@@ -12,17 +12,32 @@ document.addEventListener("DOMContentLoaded", function () {
     maxZoom: 10,
   }).addTo(map);
 
+  L.Icon.Default.mergeOptions({
+    iconUrl: "../../images/marker.png",
+    iconRetinaUrl: "../../images/marker.png",
+    shadowUrl: "../../images/marker-shadow.png",
+  });
+
   fetch("/api/trips")
     .then((response) => response.json())
     .then((data) => {
       data.forEach((trip) => {
-        let latlngs = [
-          [trip.departure_lat, trip.departure_lon],
-          [trip.arrival_lat, trip.arrival_lon],
-        ];
-        L.polyline(latlngs, { color: "blue", weight: 2 })
+        let departure = [trip.departure_lat, trip.departure_lon];
+        let arrival = [trip.arrival_lat, trip.arrival_lon];
+
+        L.polyline([departure, arrival], { color: "blue", weight: 2 })
           .addTo(map)
           .bindPopup(`${trip.airline} Flight ${trip.flight_number}`);
+
+        // Add departure marker
+        L.marker(departure)
+          .addTo(map)
+          .bindPopup(`Departure: ${trip.departure_airport}`);
+
+        // Add arrival marker
+        L.marker(arrival)
+          .addTo(map)
+          .bindPopup(`Arrival: ${trip.arrival_airport}`);
       });
     });
 });
